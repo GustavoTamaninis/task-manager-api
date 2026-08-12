705ac38d5a83a0daa490b3d95f8aa50ed1a87086
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-08-12 20:29:50 -0300

    feat: get all tasks no frontend

---
+++ src/main/resources/static/scripts/getAllTasks.js
@@ -0,0 +1,17 @@
+function getAllTasks(){
+    fetch('/tasks')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('all-tasks').innerHTML = data
+                .map(element => `
+                    <li class="list-group-item d-flex justify-content-between align-items-center py-3">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="task2">
+                            <label class="form-check-label" for="task2">${element.title}</label>
+                        </div>
+                        <button type="button" class="btn btn-outline-success btn-sm">Remove</button>
+                    </li>
+                    `)
+                .join('');
+        }   )
+}
